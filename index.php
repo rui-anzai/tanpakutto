1fd8c4554c2dcbccb088165be7ebff7fc19c4bef
anzai <18402@toba-cmt.ac.jp>
2021-02-25 21:06:11 +0900

    layoutとドッキング

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
         <link rel="stylesheet" type="text/css" href="example.css">
         <meta charset="utf-8">
         <title>タンパクっと</title>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
-         <script src="app.js" defer></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.js"></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-annotation/0.5.7/chartjs-plugin-annotation.min.js'></script>
     </head>
     <body>
       <h1>タンパクっと</h1>
-         <div class="aaaa">
+      <div class="aaaa">
 <div class="hidden_box">
 <label for="label1">選択してください</label>
 <input type="checkbox" id="label1"/>
@@ -36,27 +36,20 @@ echo "<tr><td><a href='product.php?id={$id}'>{$name}</a></td><td>{$protein}グ�
 <!--ここまで-->
 </div>
 </div>
-     <!--円グラフのclassを定義-->
-  
-     <div class="chart-wrap" style="position: relative; display: inline-block;　display:flex; width: 800px; height: 700px;">
+<!-- 円グラフのclassを定義 -->
+<div class="chart-wrap" style="position: relative; display: inline-block;　display:flex; width: 800px; height: 700px;">
      <canvas id="myPieChart"></canvas>
      </div>   
   </div>   
- 
-
-      <!--<div class="aaaa">
-      <div class="reol">-->
-          <table border="1">
+  <table border="1">
           <tbody>
           <tr><th>       
           <option value="who"></option>
-    <?php
-     
-      //配列$products          
+      <?php
       foreach($total_products as $p){
-      //
-      $total = $total . '"'. $p['total'].'",';
-      $date_time = $date_time . '"'. $p['date_time'] .'",';   
+
+      $sum = $sum . '"'. $p['sum'].'",';
+      $time = $time . '"'. $p['time'] .'",';   
       }  
       //更新ボタン
       if(isset($_POST['add'])) {
@@ -80,38 +73,94 @@ echo "<tr><td><a href='product.php?id={$id}'>{$name}</a></td><td>{$protein}グ�
           echo "";
       }
       ?>
-	   <h2><?php echo $goukei; ?>グラム摂取しました</h2>
-         <!--棒グラフの表示-->
-     <h2>直近一週間</h2>    
+     <h2>直近一週間のグラフ</h2> 
+     <h2>本日は<?php echo (int)$goukei; ?>グラム摂取しました</h2>
+         <!--棒グラフの表示-->   
       <div class="chart-container" style="position: relative; width: 950px; height: 700px;">
           <canvas id="myLineChart">ここにチャート表示</canvas>
       </div>
      <script>
-
      //.getContext('2d');はcanvasでグラフとか描画するために使う 
      var cty = document.getElementById("myLineChart").getContext('2d');
       var myLineChart = new Chart(cty, {
-        type: 'line',
+        type: 'bar',
         data: {
-             labels: [<?php echo $date_time ?>],//各棒の名前（name)
+             labels: [<?php echo $time ?>],//各棒の名前（name)
           datasets: [
             {
               label: '直近一週間のタンパク質摂取量',
-              data: [<?php echo $total ?>],//各縦棒の高さ(値段)
+              data: [<?php echo $sum ?>],//各縦棒の高さ(値段)
                
-              borderColor: "#fdbf64",
-              backgroundColor: "rgba(0,0,0,0)"
+              backgroundColor:[
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(255, 205, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(201, 203, 207, 0.2)"
+            ],
+            borderColor: [
+              "rgb(255, 99, 132)",
+              "rgb(255, 159, 64)",
+              "rgb(255, 205, 86)",
+              "rgb(75, 192, 192)",
+              "rgb(54, 162, 235)",
+              "rgb(153, 102, 255)",
+              "rgb(201, 203, 207)"
+            ],
             }
           ],
         },
         options: {
-          
-          title: {
-            display: true,
-           
+      scales: {
+        xAxes: [{
+          id : 'x軸',
+          ticks: {
+            autoSkip: true,
+            maxTicksLimit: 7 //値の最大表示数
           }
+        }],
+        yAxes: [{
+          id : 'y軸',
+        }]
+      },
+      annotation: {
+        annotations: [
+            {
+                type: 'line', // 線分を指定
+                drawTime: 'afterDatasetsDraw',
+                id: 'a-line-1', // 線のid名を指定（他の線と区別するため）
+                mode: 'horizontal', // 水平を指定
+                scaleID: 'y軸', // 基準とする軸のid名
+                value: 65.0, // 引きたい線の数値（始点）
+                endValue: 65.0, // 引きたい線の数値（終点）
+                borderColor: 'red', // 線の色
+                borderWidth: 3, // 線の幅（太さ）
+                borderDash: [2, 2],
+                borderDashOffset: 1,
+                label: { // ラベルの設定
+                    backgroundColor: 'rgba(255,255,255,0.8)',
+                    bordercolor: 'rgba(200,60,60,0.8)',
+                    borderwidth: 2,
+                    fontSize: 10,
+                    fontStyle: 'bold',
+                    fontColor: 'rgba(200,60,60,0.8)',
+                    xPadding: 10,
+                    yPadding: 10,
+                    cornerRadius: 3,
+                    position: 'left',
+                    xAdjust: 0,
+                    yAdjust: 0,
+                    enabled: true,
+                    content: '1日の目標摂取量[タンパク質]'
+                }
+            },
+
+        ]
+    }
         }
-      });      
+  });      
       </script>
       <!--index.phpにpost-->
       <form action="index.php" method="post">
