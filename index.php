282ac341bea7b563fdba6edc694aae95b8dd474c
anzai <18402@toba-cmt.ac.jp>
2021-02-26 14:05:32 +0900

    chart.jsのバージョンを2.9.3に変更

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <meta charset="utf-8">
         <title>タンパクっと</title>
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
         <script src='https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-annotation/0.5.7/chartjs-plugin-annotation.min.js'></script>
 
     </head>
